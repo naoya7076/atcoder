868fa1ff49a51111fe3892671d27035e7b808b9e
naoya7076 <naoya7076@gmail.com>
2024-03-06 23:27:04 +0900

    add comment

--- arc098_a.py
+++ arc098_a.py
@@ -1,3 +1,5 @@
+# NOTE: こちらの回答もわかりやすかった
+# https://atcoder.jp/contests/abc098/submissions/50913612
 N = int(input())
 S = list(input())
 
@@ -6,6 +8,7 @@ east_sum = [0] * (N + 1)
 
 for i in range(N):
     west_sum[i + 1] = west_sum[i] + (1 if S[i] == "W" else 0)
+    # Nは1始まりだが配列のインデックスは0始まりなので-1している。
     east_sum[N - i - 1] = east_sum[N - i] + (1 if S[N - i - 1] == "E" else 0)
 
 min_turn = float('inf')

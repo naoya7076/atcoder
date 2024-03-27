2c4b4fc1b073e24616892750dbc3065c7c341d45
naoya7076 <naoya7076@gmail.com>
2024-03-27 13:34:07 +0900

    深さ優先探索の実装

---
+++ atc001_a.py
@@ -0,0 +1,30 @@
+# 深さ優先探索
+H,W = map(int, input().split())
+c = [list(input()) for _ in range(H)]
+seen = [[False] * W for _ in range(H)]
+
+def dfs(x,y):
+    for dx,dy in [[1,0],[0,1],[-1,0],[0,-1]]:
+        nx,ny = x+dx,y+dy
+        if nx < 0 or nx >= H or ny < 0 or ny >= W:
+            continue
+        if c[nx][ny] == "#":
+            continue
+        if seen[nx][ny]:
+            continue
+        seen[nx][ny] = True
+        dfs(nx,ny)
+
+# sとgの座標を探す
+sx,sy,gx,gy = 0,0,0,0
+for h in range(H):
+    for w in range(W):
+        if c[h][w] == "s":
+            sx,sy = h,w
+        if c[h][w] == "g":
+            gx,gy = h,w
+dfs(sx,sy)
+if seen[gx][gy]:
+    print("Yes")
+else:
+    print("No")
